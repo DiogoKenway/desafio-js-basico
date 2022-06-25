312d9acb3d9bc7275c63dd87c03dacc18176daba
Diogo Alves <diogodidi2022@gmail.com>
2022-06-25 15:02:09 -0300

    desafio js basico curso devQuest

---
+++ script.js
@@ -0,0 +1,25 @@
+function somar(numero1, numero2) {
+    const resultado = numero1 + numero2
+    return `A Soma dos Dois Números é: ${resultado}`
+}
+
+function subtrair(numero1, numero2) {
+    const resultado = numero1 - numero2
+    return `O Resulta da subtração é: ${resultado}`
+}
+
+function multiplicação(numero1, numero2) {
+    const resultado = numero1 * numero2
+    return `O Resultado da multiplicação é: ${resultado}`
+}
+
+function divisao(numero1, numero2) {
+    const resultado = numero1 / numero2
+    return `O Resultado da divisão é: ${resultado}`
+}
+
+
+console.log(somar(5, 4))
+console.log(subtrair(5, 4))
+console.log(multiplicação(5, 4))
+console.log(divisao(5, 4))
